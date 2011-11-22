81ccfcca8b65f1b23965a5b2634f6b71ae7e99fe
Simon <xxx>
2011-11-22 19:03:26 +0100

    Faster

--- puzzle.py
+++ puzzle.py
@@ -29,17 +29,24 @@ class Employee():
     """Employee class """
     def __init__(self, employee_no):
         self.number = employee_no
-        self.co_worker = []
-        self.is_going = False
+        self.co_worker_list = []
+        self.original_co_worker_list = []
+        self.is_going = True
 
-    def add_co_worker(self, employee_no):
+    def add_co_worker(self, employee):
         """Method for adding an item to co_worker list"""
-        self.co_worker.append(employee_no)
+        self.co_worker_list.append(employee)
+        self.original_co_worker_list.append(employee)
 
-    def del_co_worker(self, employee_no):
+    def del_co_worker(self, employee):
         """Method for removing an item to co_worker list"""
-        self.co_worker.remove(employee_no)
-
+        self.co_worker_list.remove(employee)
+        
+    def restore_object(self):
+        """Method for restoring the object to its initial state"""
+        self.co_worker_list = self.original_co_worker_list
+        self.is_going = True
+        
 def find(func, list_seq):
     """Dynamic sarch function that returns the first item in list
        where func(item) is True."""
@@ -51,121 +58,124 @@ def reduce_list(employee_list, check_favorite = False):
     """Function for reducing the list of all employees to a minimum"""
     # Sort the list of employees by teamCount
     employee_list = sorted(employee_list,
-                          key=lambda empl : len(empl.co_worker),
+                          key=lambda empl : len(empl.co_worker_list),
                           reverse=True)
+    
+    # Introduce a list for all traveling employees
+    traveling_employee_list = []
 
     # Find a employee that's not has been checked
     if check_favorite == True:
         # If check_favorite is set, set favorite employee to be checked
-        check_employee = find(lambda item: item.number == "1009",
+        check_favorite_employee = find(lambda item: item.number == "1009",
                               employee_list)
-    else:
-        # Find employee with greatest team count that has not been checked
-        check_employee = find(lambda item: item.is_going == False,
-                              employee_list)
-
-    # If no employee is found return list. We are finished!
-    if (check_employee == None):
-        return employee_list
-
-    # Loop all co workers of the employee found
-    for co_worker in check_employee.co_worker:
-        # Find the co worker in the list of employees
-        employee = find(lambda item: item.number == co_worker, employee_list)
-
-        if employee == None:
+        
+        if check_favorite_employee != None:
+            employee_list.remove(check_favorite_employee)
+            employee_list.insert(0,check_favorite_employee)
+        
+    # Loop all employees in list
+    for employee in employee_list:
+        # Loop all co workers of the employee found
+        if employee.is_going == True:
+            # Is is_going flag set: put employee to traveling list
+            traveling_employee_list.append(employee.number)
+        else:
+            # Otherwise skip this employee
             continue
+        
+        for co_worker in employee.co_worker_list:
+            # Delete the reference to the employee
+            co_worker.del_co_worker(employee)
+            
+            # Remove the co_worker from employee_list if it has
+            # no co_workers left
+            if len(co_worker.co_worker_list) == 0:
+                co_worker.is_going = False                
 
-        # Remove the reference to the checked employee
-        # from the co-worker list
-        for co_worker2 in employee.co_worker:
-            if (co_worker2 == check_employee.number):
-                employee.del_co_worker(co_worker2)
-        # Remove this co-worker if it does not
-        # belong to any more team
-        if len(employee.co_worker) == 0:
-            employee_list.remove(employee)
-
-    # Mark this employee to be finished
-    check_employee.is_going = True
-
-    # Call this function again recursivly
-    return reduce_list(employee_list)
+    return traveling_employee_list
 
 
 def retrieve_teams():
     """Function that reads team members from file and returns them in a list """
-    print "Print the number of teams and the members on team"
-
+    #print "Print the number of teams and the members on team"
     employee_list_total = []
 
     # Read how many teams that shall be given
     stdin_input = sys.stdin.readline()
-
-    # Test if input was numeric
+    
     try:
+        # Test if input was numeric
         no_of_teams = int(stdin_input)
+        
+        input_rows = []
+        
+        # Read in all teams from stdin
+        for i in range(0, no_of_teams):
+            input_rows.append(sys.stdin.readline())
+                
     except ValueError:
         print "Error: Input must be numeric. Program will exit!"
         sys.exit()
 
-    for i in range(0, no_of_teams):
-        # Read a line from standard in
-        team_row = sys.stdin.readline()
+    for row in input_rows:
         # Split team into two members
-        team = team_row.split()
+        team = row.split()
 
         # Test if two members are given
         if len(team) != 2:
             print "Error: Two team members must be given: Program will exit!"
             sys.exit()
 
-        temp_empl = 0
-
-        # Loop both team members on row
+        temp_empl = [0, 0]
+        
+        # Loop both team members on row and check if the are in the list
         for i in range(0, 2):
-            employee_found = False
-
-            for temp_empl in employee_list_total:
-                if temp_empl.number == team[i]:
-                    employee_found = True
-                    # Add the coworker to employee coworker list
-                    if i == 0:
-                        temp_empl.add_co_worker(team[1])
-                    else:
-                        temp_empl.add_co_worker(team[0])
+            employee_found = 0
+                        
+            for temp_empl[i] in employee_list_total:
+                if temp_empl[i].number == team[i]:
+                    employee_found = 1
                     break
-
-            if employee_found == False:
+                
+            if temp_empl[i] == 0 or employee_found == 0:
                 # Employee is not found in list, add it!
-                temp_empl = Employee(team[i])
-                # Add the coworker to employee coworker list
-                if i == 0:
-                    temp_empl.add_co_worker(team[1])
-                else:
-                    temp_empl.add_co_worker(team[0])
-                employee_list_total.append(temp_empl)
+                temp_empl[i] = Employee(team[i])
+                employee_list_total.append(temp_empl[i])
+
+        i = 0                    
+        for i in range(0, 2):
+            # Add co_workers to respectivly employee
+            if i == 0:
+                temp_empl[i].add_co_worker(temp_empl[1])
+            else:
+                temp_empl[i].add_co_worker(temp_empl[0])
+                            
     # Return the list of employees
     return employee_list_total
 
-
-# Get a list of employees and make a deepcopy
-total_list1 = retrieve_teams()
-total_list2 = deepcopy(total_list1)
+# Get a list of employee
+total_list = retrieve_teams()
 
 # Reduce the list to a minimum of travelling employees
-default_list = reduce_list(total_list1)
+default_list = reduce_list(total_list)
+
+# Restore the list for another run
+for item in total_list:
+    item.restore_object()
 
 # Reduce the list to a minimum of travelling employees with 
 # considereations to the favorite employee
-favorite_list = reduce_list(total_list2, True)
+favorite_list = reduce_list(total_list, True)
 
+print len(favorite_list)
+print len(default_list)
 # Print the shortest list
 if len(favorite_list) <= len(default_list):
     print len(favorite_list)
     for item in favorite_list:
-        print item.number
+        print item
 else:
     print len(default_list)
     for item in default_list:
-        print item.number
+        print item
